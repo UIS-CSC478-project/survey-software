59211fa35346cc12e50d37ef631dd53b011a07ae
Joe Fowler <fowler0503@gmail.com>
2013-11-03 23:34:31 -0700

    Create SurvRev.java

---
+++ Code/surveysoftware/SurvRev.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class SurvRev extends JFrame {
+	public SurvRev() {
+		//initGUI();
+	}
+	public void initGUI()
+	{
+		setSize(684, 704);
+		setResizable(false);
+		getContentPane().setLayout(null);
+		setVisible(true);
+	}
+
+}
