6eeccbea0e4813ac6c931ae9106303b70a87a3ef
Joe Fowler <fowler0503@gmail.com>
2013-11-03 23:36:59 -0700

    Update SurvRef.java

--- Code/surveysoftware/SurvRef.java
+++ Code/surveysoftware/SurvRef.java
@@ -1,29 +1,85 @@
-package surveysoftware;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
-
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SurvRef extends JFrame {
-	public SurvRef() {
-		initGUI();
+	private JButton btnReviewSurvey, btnGiveSurvey, btnMainMenu;
+	private JLabel lblChooseSurveyFrom;
+	private JComboBox comboBox;
+	
+	public SurvRef() 
+	{
+		//initGUI();
 	}
 
 
 public void initGUI()
 {
-	survey_actions mysurvey = new survey_actions();
-	String[] surveyList = mysurvey.getSurveyNames();
-	setSize(436, 422);
+	setSize(428, 264);
 	setResizable(false);
 	getContentPane().setLayout(null);
 	
-	JLabel lblChooseSurveyFrom = new JLabel("Choose survey from drop down");
-	lblChooseSurveyFrom.setBounds(127, 11, 183, 14);
+	comboBox = new JComboBox();
+	comboBox.setBounds(121, 68, 152, 20);
+	getContentPane().add(comboBox);
+	
+	lblChooseSurveyFrom = new JLabel("Choose survey from drop down");
+	lblChooseSurveyFrom.setBounds(121, 27, 158, 42);
 	getContentPane().add(lblChooseSurveyFrom);
 	
-	JComboBox comboBox = new JComboBox(surveyList);
-	comboBox.setBounds(127, 36, 152, 20);
-	getContentPane().add(comboBox);
+	btnReviewSurvey = new JButton("Review Survey");
+	btnReviewSurvey.setBounds(70, 119, 118, 23);
+	getContentPane().add(btnReviewSurvey);
+	
+	btnGiveSurvey = new JButton("Give Survey");
+	btnGiveSurvey.setBounds(214, 119, 113, 23);
+	getContentPane().add(btnGiveSurvey);
+	
+	btnMainMenu = new JButton("Main Menu");
+	btnMainMenu.setBounds(156, 171, 106, 23);
+	getContentPane().add(btnMainMenu);
+	
+	btnReviewSurvey.addActionListener(new ButtonListener());
+	btnGiveSurvey.addActionListener(new ButtonListener());
+	btnMainMenu.addActionListener(new ButtonListener()); 
 }
-}
+	public class ButtonListener implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+			if(e.getSource()==btnReviewSurvey)
+			{
+				setVisible(false);
+				SurvRev svr = new SurvRev();
+				svr.initGUI();
+				
+			}
+			else if(e.getSource()==btnGiveSurvey)
+			{
+				setVisible(false);
+				SurvGive svg = new SurvGive();	
+				svg.initGUI();
+			}
+			else
+			{
+				setVisible(false);
+				Choice chc = new Choice();
+				chc.initChoice();
+			}
+		}		
+     }
+}    
+	
+
+
+    
+
+
+	
+	
+
