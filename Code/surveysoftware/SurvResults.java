4fea309c19b54ece28bafe4020a60d0db7473cd1
Joe Fowler <fowler0503@gmail.com>
2013-12-11 15:26:55 -0700

    Create SurvResults.java

---
+++ Code/surveysoftware/SurvResults.java
@@ -0,0 +1,19 @@
+package surveysoftware;
+
+import javax.swing.JFrame;
+
+
+
+public class SurvResults extends JFrame {
+	
+	public SurvResults() {
+		//initGui();
+	}
+	public void initGui(){
+		setVisible(true);
+		setSize(640, 668);
+		setResizable(false);
+	
+		
+	}
+}
